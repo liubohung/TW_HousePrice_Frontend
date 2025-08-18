64b18481d4667d61057e11d02b2377aa62322d6a
liubohung <liubohung@gmail.com>
2025-08-18 23:33:06 +0800

    Updates

---
+++ render-template.js
@@ -0,0 +1,19 @@
+export default function(ssrContext) {
+var __t, __p = '';
+__p += '<!doctype html><html ' +
+((__t = ( ssrContext._meta.htmlAttrs )) == null ? '' : __t) +
+'><head>' +
+((__t = ( ssrContext._meta.headTags )) == null ? '' : __t) +
+' <title>TW_HousePrice_frontend</title><meta charset=utf-8><meta name=description content="Visualzation Taiwan real House price registration data and other infomation."><meta name=format-detection content="telephone=no"><meta name=msapplication-tap-highlight content=no><meta name=viewport content="user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width"><link rel=icon type=image/png sizes=128x128 href=/TW_HousePrice_Frontend/icons/favicon-128x128.png><link rel=icon type=image/png sizes=96x96 href=/TW_HousePrice_Frontend/icons/favicon-96x96.png><link rel=icon type=image/png sizes=32x32 href=/TW_HousePrice_Frontend/icons/favicon-32x32.png><link rel=icon type=image/png sizes=16x16 href=/TW_HousePrice_Frontend/icons/favicon-16x16.png><link rel=icon type=image/ico href=/TW_HousePrice_Frontend/favicon.ico><script async src="https://www.googletagmanager.com/gtag/js?id=G-4BZ8Q8BCC0"></script><script>window.dataLayer = window.dataLayer || [];\n      function gtag(){dataLayer.push(arguments);}\n      gtag(\'js\', new Date());\n\n      gtag(\'config\', \'G-4BZ8Q8BCC0\');</script><script type=module crossorigin src=/TW_HousePrice_Frontend/assets/index-CRSPrEEF.js></script><link rel=stylesheet crossorigin href=/TW_HousePrice_Frontend/assets/index-DpCFgePD.css> ' +
+((__t = ( ssrContext._meta.endingHeadTags || '' )) == null ? '' : __t) +
+'</head><body class="' +
+((__t = ( ssrContext._meta.bodyClasses )) == null ? '' : __t) +
+'" ' +
+((__t = ( ssrContext._meta.bodyAttrs )) == null ? '' : __t) +
+'>' +
+((__t = ( ssrContext._meta.bodyTags )) == null ? '' : __t) +
+' <div id=q-app>' +
+((__t = ( ssrContext._meta.runtimePageContent )) == null ? '' : __t) +
+'</div></body></html>';
+return __p
+}
---
+++ server/server-entry.js
@@ -0,0 +1,1 @@
+import"vue";import{s as e}from"./assets/server-entry-DV4ePLU5.js";import"quasar";import"vue/server-renderer";import"pinia";import"vue-router";export{e as default};
